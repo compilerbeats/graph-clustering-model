8e59de80918ffdf1456fc032e1e9eeeb21d59411
compilerbeats <peter.lindner.sbg@gmail.com>
2024-02-12 15:05:13 +0100

    created data structures for random matching

--- graph-clustering-model-generate.py
+++ graph-clustering-model-generate.py
@@ -1,6 +1,7 @@
 # usage: python3 graph-clustering-model-generate.py <communities-file> <overlaps-file>
 import sys
 import numpy as np
+import copy
 
 communities_file = sys.argv[1]
 overlaps_file = sys.argv[2]
@@ -10,21 +11,21 @@ with open(communities_file, 'r') as fp:
     communities_raw = fp.readlines()
 
 # create 4-tupel for every community
-# ( community id | number of members | size of overlap | members of community as set )
-communities = {k: [k, len(communities_raw[k].replace('\n', '').split(' ')), 0,
+# ( community id | number of members | members of community as set )
+communities = {k: [k, len(communities_raw[k].replace('\n', '').split(' ')),
                    list(communities_raw[k].replace('\n', '').split(' '))] for k in range(0, len(communities_raw))}
 
 # create members structure (dict)
 # each entry members[some_id] reflects the number of memberships
 members = {}
 for i in range(0, len(communities)):
-    for member in communities[i][3]:
+    for member in communities[i][2]:
         if int(member) not in members:
             members[int(member)] = 0
 
         members[int(member)] += 1
 
-# create h(i) structure and read relevant value of our original clustering
+# create h(i, j) structure and read relevant value of our original clustering
 with open(overlaps_file) as fp:
     overlaps_raw = fp.readlines()
 
@@ -50,7 +51,30 @@ for overlaps_line in overlaps_raw:
 
 print(h_i[6][5])
 
+# create stubs for communities and members
+communities_stubs = copy.deepcopy(communities)
+
+# set number of members in community to 0, we will increase this number in the matching phase in order to reflect
+# the structure of our original communities
+for community_stub in communities_stubs.items():
+    community_stub[1][1] = 0
+    community_stub[1][2] = list()
+
+# set number of memberships to 0, we will increase this number in the matching phase in order to reflect
+# the structure of our original communities
+members_stubs = copy.deepcopy(members)
+for member_stub in members_stubs:
+    members_stubs[member_stub] = 0
+
+#### RANDOM MATCHING ####
+# first we create a structure to keep track of the communities that are connected with each other based on overlaps
+# in their members
+community_adjacency_list = dict()
+for community in communities.items():
+    community_adjacency_list[community[1][0]] = list()
+
+# we initialize our generated h(i, j) function, which should match the one of our original data
 h_i_generated = np.zeros((max_community_size + 1, max_overlap + 1))
 
-# create stubs for communities and members
-# TODO
+print(len(community_adjacency_list))
+
